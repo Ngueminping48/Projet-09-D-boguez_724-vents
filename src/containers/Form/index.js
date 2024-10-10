97a15a6dfafae8db916c6cd4b8a768055fc6ee97
Ngueminping48 <annelaurencekamenikameni@gmail.com>
2024-10-10 17:14:01 +0200

    code dans le composant form du formulaire

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -1,10 +1,13 @@
-import { useCallback, useState } from "react";
-import PropTypes from "prop-types";
-import Field, { FIELD_TYPES } from "../../components/Field";
-import Select from "../../components/Select";
-import Button, { BUTTON_TYPES } from "../../components/Button";
+import { useCallback, useState } from 'react';
+import PropTypes from 'prop-types';
+import Field, { FIELD_TYPES } from '../../components/Field';
+import Select from '../../components/Select';
+import Button, { BUTTON_TYPES } from '../../components/Button';
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 500); })
+const mockContactApi = () =>
+  new Promise((resolve) => {
+    setTimeout(resolve, 500);
+  });
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
@@ -16,6 +19,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
@@ -25,26 +29,26 @@ const Form = ({ onSuccess, onError }) => {
   );
   return (
     <form onSubmit={sendContact}>
-      <div className="row">
-        <div className="col">
-          <Field placeholder="" label="Nom" />
-          <Field placeholder="" label="Prénom" />
+      <div className='row'>
+        <div className='col'>
+          <Field placeholder='' label='Nom' />
+          <Field placeholder='' label='Prénom' />
           <Select
-            selection={["Personel", "Entreprise"]}
+            selection={['Personel', 'Entreprise']}
             onChange={() => null}
-            label="Personel / Entreprise"
-            type="large"
+            label='Personel / Entreprise'
+            type='large'
             titleEmpty
           />
-          <Field placeholder="" label="Email" />
+          <Field placeholder='' label='Email' />
           <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
-            {sending ? "En cours" : "Envoyer"}
+            {sending ? 'En cours' : 'Envoyer'}
           </Button>
         </div>
-        <div className="col">
+        <div className='col'>
           <Field
-            placeholder="message"
-            label="Message"
+            placeholder='message'
+            label='Message'
             type={FIELD_TYPES.TEXTAREA}
           />
         </div>
@@ -56,11 +60,11 @@ const Form = ({ onSuccess, onError }) => {
 Form.propTypes = {
   onError: PropTypes.func,
   onSuccess: PropTypes.func,
-}
+};
 
 Form.defaultProps = {
   onError: () => null,
   onSuccess: () => null,
-}
+};
 
 export default Form;
